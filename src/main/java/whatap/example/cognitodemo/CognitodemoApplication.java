2d82f3991cb7fb00508ee58611b2f168597de817
oolralra <oolralra@gmail.com>
2024-01-19 09:14:19 +0900

    Add files via upload

---
+++ src/main/java/whatap/example/cognitodemo/CognitodemoApplication.java
@@ -0,0 +1,13 @@
+package whatap.example.cognitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitodemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/whatap/example/cognitodemo/Controller/BasicController.java
@@ -0,0 +1,29 @@
+package whatap.example.cognitodemo.Controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    public String Home(Model model, Authentication authentication){
+        if (authentication != null && authentication.isAuthenticated()) {
+            String email = makeEmail(authentication);
+            model.addAttribute("email", email);
+            return "result";
+        }
+        return "home";
+    }
+
+    public String makeEmail(Authentication authentication){
+        String principal =authentication.getPrincipal().toString();
+        int principalEmailIndex = principal.indexOf("email=");
+        String email = principal.substring(principalEmailIndex+6, principal.length()-2);
+        return email;
+    }
+}
